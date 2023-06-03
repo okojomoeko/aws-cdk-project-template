0b64a2981a852d94bc0089d9a8cbb30476328fec
okojomoeko <chikoro.size@gmail.com>
2023-06-03 09:30:01 +0000

    プロジェクトのテンプレート完成

--- test/aws-cdk-project-template.test.ts
+++ test/aws-cdk-project-template.test.ts
@@ -1,17 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
+import { Template } from 'aws-cdk-lib/assertions';
 import * as AwsCdkProjectTemplate from '../lib/aws-cdk-project-template-stack';
 
 test('SQS Queue and SNS Topic Created', () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new AwsCdkProjectTemplate.AwsCdkProjectTemplateStack(app, 'MyTestStack');
+  const stack = new AwsCdkProjectTemplate.AwsCdkProjectTemplateStack(
+    app,
+    'MyTestStack'
+  );
   // THEN
 
   const template = Template.fromStack(stack);
 
   template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
+    VisibilityTimeout: 300,
   });
   template.resourceCountIs('AWS::SNS::Topic', 1);
+  expect(template.toJSON()).toMatchSnapshot();
 });
