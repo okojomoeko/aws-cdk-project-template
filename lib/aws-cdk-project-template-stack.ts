75c79d1f5243c3bf4e078eb85ac62b882face900
okojomoeko <chikoro.size@gmail.com>
2024-06-10 21:46:37 +0900

    add: biome migrate from eslint

--- lib/aws-cdk-project-template-stack.ts
+++ lib/aws-cdk-project-template-stack.ts
@@ -1,15 +1,15 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Stack, type StackProps } from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import type { Construct } from 'constructs';
 
 export class AwsCdkProjectTemplateStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const queue = new sqs.Queue(this, 'AwsCdkProjectTemplateQueue', {
-      visibilityTimeout: Duration.seconds(300)
+      visibilityTimeout: Duration.seconds(300),
     });
 
     const topic = new sns.Topic(this, 'AwsCdkProjectTemplateTopic');
